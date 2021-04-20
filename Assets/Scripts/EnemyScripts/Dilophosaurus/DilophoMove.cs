92210e129e7bd4f61d539da63208c8d02ebc5c70
nahigles <nahigles@ucm.es>
2021-04-21 00:13:27 +0200

    Ataque veneno dillofosaurio sin daño

---
+++ Assets/Scripts/EnemyScripts/Dilophosaurus/DilophoMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DilophoMove : MonoBehaviour
+{
+    [SerializeField]
+    GameObject jugador = null;
+    [SerializeField]
+    private float velocity;
+    [SerializeField]
+    private float standByTime;
+    Rigidbody2D rbEnemigo;
+    private Vector2 distancia;
+    bool dentroRango = false;
+    [SerializeField]
+    public GameObject drop;
+    Perception perception;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbEnemigo = GetComponent<Rigidbody2D>();
+        Instantiate(drop, this.transform.position, this.transform.rotation);
+        perception = GetComponentInChildren<Perception>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (transform.position != jugador.transform.position && !perception.GetSee())
+        {
+            distancia = jugador.transform.position - transform.position;
+           
+        }
+        else if (transform.position != jugador.transform.position && perception.GetSee())
+        {
+            distancia = jugador.transform.position + transform.position;
+        }
+        Invoke(nameof(Mueve), standByTime);
+    }
+
+    private void Mueve()
+    {
+        rbEnemigo.velocity = distancia.normalized * (velocity);
+        transform.up = distancia.normalized;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        dentroRango = true;
+    }
+
+}
+
---
+++ Assets/Scripts/EnemyScripts/Dilophosaurus/PoisonAttack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonAttack : MonoBehaviour
+{
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    public float poisonSpeed = 2f;
+    [SerializeField]
+    public float poisonDistance = 6f;
+    [SerializeField]
+    public GameObject drop;
+    float tiempolanzado;
+    float tiempotop;
+    Rigidbody2D rg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = this.GetComponent<Rigidbody2D>();
+        tiempolanzado = Time.time;
+        tiempotop = poisonDistance / poisonSpeed;//siguiendo la formula v=d/t        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time - tiempolanzado < tiempotop)
+        {
+            if (Time.time - tiempolanzado < poisonDistance / 2)
+                rg.velocity = (transform.up * poisonSpeed);
+            else
+                rg.AddForce(-transform.up * poisonSpeed / 4);
+
+
+        }
+        else
+        {
+            Destroy(this.gameObject);
+
+
+        }
+
+    }
+    void OnDestroy()
+    {
+        Instantiate(drop, this.transform.position, this.transform.rotation);
+
+    }
+    
+}
