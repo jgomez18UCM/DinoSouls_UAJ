5ad6e20e1926c042fbe2fc5b3780adb391033e7f
nahigles <nahigles@ucm.es>
2021-04-26 17:15:10 +0200

    Percepción funcionando

--- Assets/Scripts/EnemyScripts/Dilophosaurus/DilophoMove.cs
+++ Assets/Scripts/EnemyScripts/Dilophosaurus/DilophoMove.cs
@@ -11,40 +11,50 @@ public class DilophoMove : MonoBehaviour
     float velocAux;
     [SerializeField]
     private float standByTime;
+    [SerializeField]
     Rigidbody2D rbEnemigo;
     private Vector2 distancia, vEnemigo;
     [SerializeField]
     public GameObject drop;
     [SerializeField]  
     float distanciaMod;
+    [SerializeField]
     Perception perception;
     [SerializeField]
-    BoxCollider2D rangoAcercar, percepCol;
+    CircleCollider2D rangoAcercar, percepCol;
+    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rbEnemigo = GetComponent<Rigidbody2D>();
+       // rbEnemigo = GetComponent<Rigidbody2D>();
         Instantiate(drop, this.transform.position, this.transform.rotation);
-        perception = GetComponentInChildren<Perception>();
+        //perception = GetComponentInChildren<Perception>();
         velocAux = velocity;
     }
     void FixedUpdate()
     {
-        rbEnemigo.velocity = vEnemigo;
+       // rbEnemigo.velocity = vEnemigo;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        distancia = jugador.transform.position - transform.position;
+        //distancia = jugador.transform.position - transform.position;
         //Invoke(nameof(Movimiento), standByTime);
     }
    
     private void Movimiento()
     {
+        
+        distancia = jugador.transform.position - transform.position;
+        rbEnemigo.velocity = distancia.normalized *velocity;
+        Debug.Log(rbEnemigo.velocity);
+
+        if (percepCol) percepCol.enabled = false;
+        if (perception) perception.enabled = false;
         /*if (!perception.GetSee())
         {
             vEnemigo = distancia.normalized * (velocity);
@@ -57,17 +67,26 @@ public class DilophoMove : MonoBehaviour
         }*/
 
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (percepCol) percepCol.enabled = true;
+        if (perception) perception.enabled = true;
+        
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        percepCol.enabled = false;
+        Movimiento();
+        
+        /* percepCol.enabled = false;
         if (perception.GetSee())
         {
             velocAux = 0;
         }
-        velocAux = velocity;
+        velocAux = velocity;*/
 
     }
-    private void OnTriggerStay2D(Collider2D collider)
+    /*private void OnTriggerStay2D(Collider2D collider)
     {
         transform.up = distancia.normalized;
         if (perception.GetSee())
@@ -80,7 +99,7 @@ public class DilophoMove : MonoBehaviour
         }
        
 
-    }
+    }*/
 }
 
 
