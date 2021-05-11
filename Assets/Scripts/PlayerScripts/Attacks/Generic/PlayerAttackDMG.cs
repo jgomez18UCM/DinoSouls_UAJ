afc528149bda30737218c611530a35156dbb15bf
jgomez18UCM <jgomez18@ucm.es>
2021-05-11 17:23:50 +0200

    Veneno de la lanza funcional

--- Assets/Scripts/PlayerScripts/Attacks/Generic/PlayerAttackDMG.cs
+++ Assets/Scripts/PlayerScripts/Attacks/Generic/PlayerAttackDMG.cs
@@ -6,15 +6,38 @@ public class PlayerAttackDMG : MonoBehaviour
 {
     [SerializeField]
     private int damage;
+    [SerializeField]
+    private int poisonTicks;
+    [SerializeField]
+    private int poisonDmgPerTick;
+
+    private bool veneno = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            collision.gameObject.GetComponent<EnemyDamage>().TakeDamage(damage);
+            EnemyDamage enemigo = collision.gameObject.GetComponent<EnemyDamage>();
+           
+            
+                enemigo.TakeDamage(damage);
+            
+            if(veneno)
+            {
+                Debug.Log("envenenando");
+                
+                enemigo.Poisoned(poisonTicks, poisonDmgPerTick);
+            }
                 
         }
 
-        Debug.Log("hola buenas tardes");
+       // Debug.Log("hola buenas tardes");
+    }
+
+    public void ActivatePoison()
+    {
+        veneno = true;
+
     }
 }
---
+++ Assets/Scripts/Upgrades/SpearUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpearUpgrade : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player)
+        {
+            player.ActivarVenenoLanza();
+        }
+        else Debug.LogWarning("Mejora de la lanza colisionando con un objeto no jugador");
+    }
+}
