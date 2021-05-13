f324da1c1486f728267d1649ce96435a005ca312
Sebana02 <sebana@ucm.es>
2021-05-13 22:57:57 +0200

    Añadido el menu inicial

---
+++ Assets/Scripts/Managers/menuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject controls;
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void changeScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void ShowHideControls()
+    {
+        if (controls.activeInHierarchy == true) controls.SetActive(false);
+        else controls.SetActive(true);
+    }
+}
