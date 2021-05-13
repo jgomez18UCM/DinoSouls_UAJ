a4a365847f34dd15dcbb99d34c8c3559211376cc
Bimbloc <82374311+Bimbloc@users.noreply.github.com>
2021-05-13 22:00:16 +0200

    Piedras PROGRAMADAS(bimbloc=Rocio)

---
+++ Assets/Scripts/Piedras.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Piedras : MonoBehaviour
+{
+    [SerializeField]
+    Image piedra=null;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            piedra.enabled = true;
+            Invoke("PasaPiedra", 3);
+           
+        
+        }
+    
+    }
+    void PasaPiedra()
+    {
+        piedra.enabled = false;
+        Destroy(this.gameObject);
+
+    }
+}
