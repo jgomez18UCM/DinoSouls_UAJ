5028629ec93d4ce7c0bff7a0a138b9a831240148
jgomez18UCM <jgomez18@ucm.es>
2021-05-18 18:58:14 +0200

    Creado el prefab y el sprite de la mejora de la lanza

--- Assets/Scripts/Upgrades/SpearUpgrade.cs
+++ Assets/Scripts/Upgrades/SpearUpgrade.cs
@@ -4,10 +4,12 @@ public class SpearUpgrade : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         PlayerController player = collision.GetComponent<PlayerController>();
         if (player)
         {
             player.ActivarVenenoLanza();
+            Destroy(gameObject);
         }
         else Debug.LogWarning("Mejora de la lanza colisionando con un objeto no jugador");
     }
