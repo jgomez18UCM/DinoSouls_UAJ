bfc16f9980d1922f75370949b7a6fad20161ca9c
Bimbloc <rocisa08@ucm.es>
2021-05-19 20:23:15 +0200

    Piedra que se quita al pulsar un boton

--- Assets/Scripts/Piedras.cs
+++ Assets/Scripts/Piedras.cs
@@ -7,22 +7,32 @@ public class Piedras : MonoBehaviour
 {
     [SerializeField]
     Image piedra=null;
+    PlayerController player;
 
-    [SerializeField]
-    float time = 3;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerController player = other.GetComponent<PlayerController>();
+       player = other.GetComponent<PlayerController>();
 
         if (player != null)
         {
             piedra.enabled = true;
-            Invoke("PasaPiedra", time);
-            player.ActivaStunt(time);
+            player.enabled = false;
+            Time.timeScale = 0;
+           
+           
+            //player.ActivaStunt(time);
         }
+    }
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Invoke("PasaPiedra", 0);
     }
     void PasaPiedra()
     {
+        Time.timeScale = 1;
+        player.enabled = true;
         piedra.enabled = false;
     }
 }
