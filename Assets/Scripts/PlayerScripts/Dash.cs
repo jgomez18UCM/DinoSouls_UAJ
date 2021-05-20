935a36112473892622c2a58a01dc47712eb5d589
rodsan05 <rodsan05@ucm.es>
2021-05-20 19:32:41 +0200

    Trail dash y powerup

--- Assets/Scripts/PlayerScripts/Dash.cs
+++ Assets/Scripts/PlayerScripts/Dash.cs
@@ -21,6 +21,12 @@ public class Dash : MonoBehaviour
     [Tooltip("GO hijo del player que indica la dirección del dash")]
     private GameObject dashDirection;
 
+    [SerializeField]
+    private GameObject[] trailsDash;
+
+    //Variable que dice cuál de los dos trails se usa para el dash, dependiendo de si hay o no power up
+    private int dashTrailID = 0;
+
     //Script del movimiento de la cámara
     private CameraShake cameraShake;
     private RandomCameraShake randomShake;
@@ -73,13 +79,26 @@ public class Dash : MonoBehaviour
         //Movimiento de cámara
         cameraShake.Shake(-direction, 0.1f, 0.1f);
 
+        //Se activa el trail
+        trailsDash[dashTrailID].SetActive(true);
+        trailsDash[dashTrailID].GetComponentInChildren<ParticleSystem>().Play();
+        CancelInvoke();
+        Invoke("DeactivateTrail", dashTime + 1);
+
         //Reproducción del sfx
         dashSound.Play();
     }
+
     public void Upgrade()
     { 
         dashDistance = dashDistance * 2;
         dashSpeed = dashSpeed * 2;
-    
+
+        dashTrailID = 1;
+    }
+
+    void DeactivateTrail() 
+    {
+        trailsDash[dashTrailID].SetActive(false);
     }
 }
