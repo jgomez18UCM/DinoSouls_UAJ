1331ce815a15ce1552a2b56ae69655291d1861f7
jgomez18UCM <jgomez18@ucm.es>
2021-05-20 18:51:34 +0200

    Maquina de estados para los patrones del Carnotauro creados

---
+++ Assets/Scripts/EnemyScripts/Carnotaurus/CarnotaurusPatrons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarnotaurusPatrons : MonoBehaviour
+{
+    enum States {Sleep, First, Close, Far};
+
+    States state;
+    [SerializeField]
+    CarnotaurusBite bite;
+    CarnotaurusCarga charge;
+
+    private void Start()
+    {
+        state = States.Sleep;
+        charge = GetComponent<CarnotaurusCarga>();
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case States.First:
+
+                break;
+            case States.Far:
+                break;
+            case States.Close:
+                break;
+            default:
+                break;
+        }
+    }
+
+    //Este método activa por primera vez al Carnotauro
+    public void WakeUp()
+    {
+        state = States.First;
+    }
+
+    
+}
