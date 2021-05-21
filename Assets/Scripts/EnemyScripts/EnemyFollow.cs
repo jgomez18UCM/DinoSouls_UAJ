1fb1c608f28214394b99d1bbb9f3df314e96dc0d
Sebana02 <sebana@ucm.es>
2021-05-21 21:23:54 +0200

    Zona 1 decorada y con enemigos

--- Assets/Scripts/EnemyScripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyScripts/EnemyFollow.cs
@@ -34,7 +34,6 @@ public class EnemyFollow : MonoBehaviour
         {
             distancia = jugador.transform.position - transform.position;
         }
-        Mueve();
     }
 
     void Update()
