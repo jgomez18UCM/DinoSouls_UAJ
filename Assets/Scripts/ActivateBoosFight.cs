83fa7dea556a68c7dba1f845937841eea46763be
rodsan05 <rodsan05@ucm.es>
2021-05-23 12:05:48 +0200

    Activación carnotauro y cierre de zona

---
+++ Assets/Scripts/ActivateBoosFight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateBoosFight : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject carnotauro;
+    [SerializeField]
+    private AudioSource rugido;
+
+    [SerializeField]
+    private GameObject piedrasEntrada;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Rugido
+        //rugido.Play();
+
+        carnotauro.SetActive(true);
+        piedrasEntrada.SetActive(true);
+    }
+}
